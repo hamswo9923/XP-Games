e148554842d03eb50a2c395f25477b926346a1e6
Maxim (UnderPlayer.) <maximvanin3@gmail.com>
2026-03-09 18:08:48 +0300

    Change Firebase API key and app ID to 'Null'
    Updated Firebase configuration values to 'Null'.

--- XP/School-RPG/modern/firebase.js
+++ XP/School-RPG/modern/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit, doc, getDoc, setDoc, updateDoc, startAfter } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAyatGz9z_EYXBAFrH2wrnX8snbDn1ESJk",
+    apiKey: "Null",
     authDomain: "schoolrpg-leaderboard.firebaseapp.com",
     projectId: "schoolrpg-leaderboard",
     storageBucket: "schoolrpg-leaderboard.firebasestorage.app",
     messagingSenderId: "942057524924",
-    appId: "1:942057524924:web:4720f94710edd210ec8ab9",
+    appId: "null",
     measurementId: "G-MR13BVCGZW"
 };
 
